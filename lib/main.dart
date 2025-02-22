dc6660d88278779efaf3793b46056b9580d37bb9
yibeltal-gashaw <yibeltalgashaw320@.com>
2025-02-22 18:32:21 +0300

    Landing Page Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:land_house_verify/pages/landing/landing_page.dart';
 import 'package:land_house_verify/service_locator.dart';
 import 'package:land_house_verify/services/login_or_register.dart';
 import 'package:land_house_verify/themes/themes_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -39,8 +41,46 @@ class MyApp extends StatelessWidget {
 
       theme: Provider.of<ThemeProvider>(context).themeData,
 
-      home: LoginOrRegister(),
+      home: InitialScreen(),
        // Initial screen for user authentication
+    );
+  }
+}
+
+class InitialScreen extends StatelessWidget {
+  const InitialScreen({Key? key}) : super(key: key);
+
+  Future<String> determineStartPage() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isFirstTime = prefs.getBool('isFirstTime') ?? true;
+
+    if (isFirstTime) {
+      // Set isFirstTime to false only if routing to the landing page
+      prefs.setBool('isFirstTime', false);
+      return 'landing';
+    } else {
+      return 'login';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<String>(
+      future: determineStartPage(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasData) {
+          switch (snapshot.data) {
+            case 'landing':
+              return LandingPage();
+            default:
+              return LoginOrRegister();
+          }
+        } else {
+          return Center(child: Text('Error loading app'));
+        }
+      },
     );
   }
 }
---
+++ lib/pages/landing/landing_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:land_house_verify/services/login_or_register.dart';
+
+import 'landing_page_container.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        children: [
+          landingPageContainer(
+              imagepath:'assets/images/img1.png',
+              title: 'Welcome to the Photo Gallery',
+              subtitle:'Explore stunning images at your fingertips.',
+              context: context
+          ),
+          landingPageContainer(
+              imagepath:'assets/images/img2.png',
+              title: 'View High-Quality Images',
+              subtitle:'Tap on any image to view it in fullscreen.',
+              context: context
+          ),
+          landingPageContainer(
+              imagepath:'assets/images/img3.png',
+              title:'Get Started Now!',
+              subtitle:'Click the button below to explore the gallery.',
+              context: context,
+              ontap: () async {
+                if(context.mounted){
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginOrRegister()),
+                  );
+                }
+              },
+              haveButton: true
+          ),
+        ],
+      ),
+    );
+  }
+}
